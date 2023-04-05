13c020b5a4b493bd495d4df6c2f7d238cf18e164
Emkay Nguyen <81498410+DiningSystem@users.noreply.github.com>
2023-04-05 13:28:45 -0500

    Update gen_images.py

--- diffusion-projected-gan/gen_images.py
+++ diffusion-projected-gan/gen_images.py
@@ -73,7 +73,7 @@ def make_transform(translate: Tuple[float,float], angle: float):
 @click.option('--seeds', type=parse_range, help='List of random seeds (e.g., \'0,1,4-6\')', required=True)
 @click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
 @click.option('--class', 'class_idx', type=int, help='Class label (unconditional if not specified)')
-@click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
+@click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='random', show_default=True)
 @click.option('--translate', help='Translate XY-coordinate (e.g. \'0.3,1\')', type=parse_vec2, default='0,0', show_default=True, metavar='VEC2')
 @click.option('--rotate', help='Rotation angle in degrees', type=float, default=0, show_default=True, metavar='ANGLE')
 @click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
